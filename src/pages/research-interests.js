ab995624cc0388606e7f24390e58ded65211da99
Tinzzy Li <tinzzy.li@gmail.com>
2025-10-24 01:41:32 -0500

    update research interest, fix layout bug that blocking title in small page width

--- src/pages/research-interests.js
+++ src/pages/research-interests.js
@@ -8,16 +8,21 @@ export default function ResearchInterests() {
       <p className="content" style={{fontFamily: "var(--font-mono)", fontSize: "var(--smaller-text-font-size)" , fontWeight: 400}}>
         <br />
 
-        i am broadly interested in <span style={{textDecoration: "underline", fontStyle: "italic"}}>artificial intelligence</span> and <span style={{textDecoration: "underline", fontStyle: "italic"}}>software engineering</span>, particularly at the intersection 
-        of the two. </p> 
+        i am broadly interested in <span style={{textDecoration: "underline"}}>Efficient Machine Learning Systems</span> during the <span style={{textDecoration: "underline"}}>Inference</span> stage.
 
-      <p className="content" style={{fontFamily: "var(--font-mono)", fontSize: "var(--smaller-text-font-size)" , fontWeight: 400}}>more specifically, i focus on enhancing 
-        the efficiency and robustness of deep learning systems 
-        against malicious inputs, as well as leveraging large 
-        language models for software testing.</p>
+        <br />
+        <br />
+
+        <p className="content" style={{fontFamily: "var(--font-mono)", fontSize: "var(--smaller-text-font-size)" , fontWeight: 400}}>specifically, my research 
+          focuses on analyzing the efficiency of deep learning pipeline systems under malicious inputs, as well as on the machine learning 
+          compilation process, including compiler design and automated kernel generation.</p>
+
+       <span style={{fontStyle: "italic"}}>keywords: efficient ml; mlsys</span> 
+          
+        </p> 
 
       <p className="content" style={{fontFamily: "var(--font-mono)", fontSize: "var(--smaller-text-font-size)" , fontWeight: 400, color: "var(--highlight-font-color)"}}>
-        *** i am actively looking for internship opportunities in these areas starting from summer 2026.
+        *** i am actively looking for internship opportunities in <span style={{textDecoration: "underline"}}>Machine Learning Compilation</span> starting from summer 2026.
       </p>
       <br />
       <br />
