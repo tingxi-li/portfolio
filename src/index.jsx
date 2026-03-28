a8662d148c5ca3157c0af300678bcb20dae09f51
Tinzzy Li <tinzzy.li@gmail.com>
2026-03-27 23:15:52 -0500

    refactor: update index.jsx imports for split CSS, remove antd

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,9 @@
-// /src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
-import 'antd/dist/reset.css';
-import "./global.css"; // <- REQUIRED: make sure this line exists
-import App from "./App"; // <- REQUIRED: make sure this line exists
-
+import "./css/fonts.css";
+import "./css/variables.css";
+import "./css/global.css";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <>
-    <App />
-  </>
-);
+root.render(<App />);
