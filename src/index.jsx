fe459c3807f233f2f17bc692482d5a1e430075e0
Tinzzy Li <tinzzy.li@gmail.com>
2026-03-28 03:33:08 -0500

    feat: simplify index.html and index.jsx for light-only brutalist theme

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./css/fonts.css";
 import "./css/variables.css";
-import "./css/global.css";
+import "./css/layout.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
